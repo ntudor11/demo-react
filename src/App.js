e59d1c773f82e5c08a5534c58a35d0bc5afcc12e
nTudor0911 <40658894+ntudor11@users.noreply.github.com>
2019-04-11 12:55:17 +0200

    Setting and passing properties

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header/>
+        <Content/>
+        <h1>Hello, {this.props.name}</h1>
+        <p>Array: {this.props.array}</p>
+        <p>Bool: {this.props.propBool ? "true..." : "False..."}</p>
+        <p>Function: {this.props.propFunc(3)}</p>
+        <p>Number: {this.props.propNumber}</p>
+        <p>String {this.props.propString}</p>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  name: PropTypes.string,
+  propArray: PropTypes.array.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propFunc: PropTypes.func,
+  propNumber: PropTypes.number,
+  propString: PropTypes.string,
+};
+
+App.defaultProps = {
+  name: 'My React App',
+  propArray: [1, 2, 3, 4, 5],
+  propBool: true,
+  propFunc: function(e) {
+    return e;
+  },
+  propNumber: 1,
+  propString: 'String value',
+};
+
+class Header extends App {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>The text</p>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
